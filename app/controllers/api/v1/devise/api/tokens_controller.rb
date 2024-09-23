17d50b511a62ddee8bc0030fec7966cf4a137d3d
Serhii Hiba <serhii.hiba@gmail.com>
2024-09-23 15:31:50 +0300

    remove V1 in tokens controller

--- app/controllers/api/v1/devise/api/tokens_controller.rb
+++ app/controllers/api/v1/devise/api/tokens_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # rubocop:disable Metrics/ClassLength
-module Api::V1::Devise
+module Devise
   module Api
     class TokensController < Devise.api.config.base_controller.constantize
       skip_before_action :verify_authenticity_token, raise: false
